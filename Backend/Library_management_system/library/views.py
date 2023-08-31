04d8006bf7dc33736f0a2772d67afb6c25d5fcc6
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2023-08-31 19:33:37 +0530

    frontend completed

--- Backend/Library_management_system/library/views.py
+++ Backend/Library_management_system/library/views.py
@@ -20,18 +20,18 @@ class Signup(APIView):
         print(name,email,pass1,pass2)
         if pass1==pass2:
             if User.objects.filter(email=email).exists():
-                return Response({"msg":"email already registered please login"},status=status.HTTP_400_BAD_REQUEST)
+                return Response({"msg":"Email already registered please sign in"},status=status.HTTP_400_BAD_REQUEST)
             elif User.objects.filter(username=name).exists():
-                return Response({"msg":"username exists please use different username"},status=status.HTTP_400_BAD_REQUEST)
+                return Response({"msg":"Username exists please use different username"},status=status.HTTP_400_BAD_REQUEST)
             else:
                 usr=User.objects.create_user(username=name,email=email,password=pass1)
                 usr.save()
                 dic=request.data
                 usrinfo=UserInfo.objects.create(user=usr,name=dic['name'],phone=dic['phone'],email=email,address=dic['address'],college=dic['college'])
                 usrinfo.save()
-                return Response({"msg":"user created"},status=status.HTTP_202_ACCEPTED)
+                return Response({"msg":"User created"},status=status.HTTP_202_ACCEPTED)
         else:
-            return Response({"msg":"password not matched"},status=status.HTTP_400_BAD_REQUEST)
+            return Response({"msg":"Password not matched"},status=status.HTTP_400_BAD_REQUEST)
         
 
 class Login(APIView):
@@ -43,16 +43,16 @@ class Login(APIView):
         usr=auth.authenticate(username=name,password=password)
         if usr is not None:
             refresh = RefreshToken.for_user(usr)
-            return Response({"msg":"user logged in", 'token':{'refresh': str(refresh),'access': str(refresh.access_token)}},status=status.HTTP_202_ACCEPTED)
+            return Response({"msg":"User logged in", 'token':{'refresh': str(refresh),'access': str(refresh.access_token)}},status=status.HTTP_202_ACCEPTED)
         else:
-            return Response({"msg":"please enter valid credentials"},status=status.HTTP_400_BAD_REQUEST)
+            return Response({"msg":"Please enter valid credentials"},status=status.HTTP_400_BAD_REQUEST)
 
 class Logout(APIView):
     authentication_classes=[]
     permission_classes=[]
     def get(self,request):
         auth.logout(request)
-        return Response({"msg":"logout successfull"},status=status.HTTP_202_ACCEPTED)
+        return Response({"msg":"Logout successfull"},status=status.HTTP_202_ACCEPTED)
 
 
 
@@ -83,12 +83,12 @@ class AddBook(ModelViewSet):
             ser=UserBookSerializer(ins,data=request.data,context={'request':request,'number':lst[0]['number']+1})  # then update the data
             if ser.is_valid():
                 ser.save()
-                return Response({"msg":"book added to cart successfully"},status=status.HTTP_202_ACCEPTED)
+                return Response({"msg":"Book added to cart successfully"},status=status.HTTP_202_ACCEPTED)
         else:   # if book is not present in cart then simply save it in database with its count 1
             ser=UserBookSerializer(data=request.data,context={'request':request,'number':1})
             if ser.is_valid():
                 ser.save()
-                return Response({"msg":"book added to cart successfully"},status=status.HTTP_202_ACCEPTED)
+                return Response({"msg":"Book added to cart successfully"},status=status.HTTP_202_ACCEPTED)
         return Response({"msg":"Invalid Request"},status=status.HTTP_400_BAD_REQUEST)
     
     def destroy(self, request,pk):
@@ -99,21 +99,12 @@ class AddBook(ModelViewSet):
         if quant==1:
             ins=UserBook.objects.get(id=pk)
             ins.delete()
-            return Response({"msg":"item removed"},status=status.HTTP_202_ACCEPTED)
+            return Response({"msg":"Item removed"},status=status.HTTP_202_ACCEPTED)
         else:
             ins=UserBook.objects.get(id=pk)
             # print(ins,quant)
             ser=UserBookSerializer(ins,data=request.data,partial=True,context={'request':request,'number':quant-1})
             if ser.is_valid():
                 ser.save()
-                return Response({"msg":"item removed"},status=status.HTTP_202_ACCEPTED)
+                return Response({"msg":"Item removed"},status=status.HTTP_202_ACCEPTED)
             return Response({"msg":"Invalid Response"},status=status.HTTP_400_BAD_REQUEST)
-
-
-
-            
-
-
-    
-
-        
